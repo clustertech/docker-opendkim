dab8fe7fff78d74618a110cd4eb00eb0a5f25e62
Clustertech sysadmin <sysadmin@clustertech.com>
2021-11-17 09:03:28 +0800

    delete all pid files before starting deamons

--- startopendkim.sh
+++ startopendkim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -f /var/run/*.pid /run/*.pid
 cp -L /etc/opendkim/keys-temp/* /etc/opendkim-keys
 chown -R opendkim:opendkim /etc/opendkim-keys/
 chmod -R g-rwx,o-rwx /etc/opendkim-keys/
